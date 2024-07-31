fe3cf2b697bd1b82ae97a2bff3a4545f1aa49d5a
Claudio Matheus <165329797+ClaudioMatheusDev@users.noreply.github.com>
2024-07-31 05:34:50 -0300

    Projeto de movimento ponto

---
+++ Movimento ponto/Movimento ponto/Ponto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Ponto
+{
+    private int x;
+    private int y;
+
+    public Ponto(int xInicial, int yInicial)
+    {
+        x = xInicial;
+        y = yInicial;
+    }
+
+    public void MoverEsquerda(int quantidade)
+    {
+        x -= quantidade;
+    }
+
+    public void MoverDireita(int quantidade)
+    {
+        x += quantidade;
+    }
+
+    public void MoverCima(int quantidade)
+    {
+        y += quantidade;
+    }
+
+    public void MoverBaixo(int quantidade)
+    {
+        y -= quantidade;
+    }
+
+    public void ExibirCoordenadas()
+    {
+        Console.WriteLine("Coordenadas finais do ponto: \n" + "x: " + x + "\n" + "y: " + y);
+    }
+}
---
+++ Movimento ponto/Movimento ponto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movimento_ponto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a coordenada x:");
+            int xInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a coordenada y:");
+            int yInicial = int.Parse(Console.ReadLine());
+
+            Ponto ponto = new Ponto(xInicial, yInicial);
+
+            Console.WriteLine("Digite o valor de movimento para a esquerda:");
+            int movimentoEsquerda = int.Parse(Console.ReadLine());
+            ponto.MoverEsquerda(movimentoEsquerda);
+
+            Console.WriteLine("Digite o valor de movimento para a direita:");
+            int movimentoDireita = int.Parse(Console.ReadLine());
+            ponto.MoverDireita(movimentoDireita);
+
+            Console.WriteLine("Digite o valor de movimento para cima:");
+            int movimentoCima = int.Parse(Console.ReadLine());
+            ponto.MoverCima(movimentoCima);
+
+            Console.WriteLine("Digite o valor de movimento para baixo:");
+            int movimentoBaixo = int.Parse(Console.ReadLine());
+            ponto.MoverBaixo(movimentoBaixo);
+
+            ponto.ExibirCoordenadas();
+        }
+    }
+}
